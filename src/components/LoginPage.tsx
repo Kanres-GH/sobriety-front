4ec92b8d58e2033ba804831220f569a15b667bef
Kanres-GH <ktnrgs1817@gmail.com>
2024-11-15 00:16:53 +0500

    added login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,8 +1,36 @@
+import { Link } from 'react-router-dom';
+import logo from '../static/imgs/logo.svg'
+import '../static/css/reg-log.css'
+
 export default function LoginPage() {
 
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className='reg-log'>
+            <div className="reg-log-img" />
+            <div className="reg-log-main">
+                <div className="reg-log-main-content">
+                    <img src={logo} alt="New Me Logo" style={{width: '40vw', alignSelf: 'center', minWidth: '350px'}} />
+                    <hr style={{display: 'block', borderColor: '#ffffff', border: 0, height: '1px', borderTop: '1px solid #1c1c1c'}}/>
+                    <div className="reg-log-main-content-heading">
+                        <h1 style={{fontWeight: '500'}}>Log into New Me</h1>
+                    </div>
+                    <div className="reg-log-main-content-form">
+                        <form>
+                            <div className="reg-log-main-content-form-input">
+                                <label htmlFor="text">Username or Email:</label>
+                                <input type="text" />
+                            </div>
+                            <div className="reg-log-main-content-form-input">
+                                <label htmlFor="password">Password:</label>
+                                <input type="password" />
+                            </div>
+                        </form>
+                    </div>
+                    
+                    <button>Sign in</button>
+                    <p>Don't have an account? <Link to='/register'><span>Sign up</span></Link></p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/RegisterPage.tsx
+++ src/components/RegisterPage.tsx
@@ -1,18 +1,18 @@
 import logo from '../static/imgs/logo.svg'
 import '../static/css/reg-log.css'
+import { Link } from 'react-router-dom';
 
 export default function RegisterPage() {
 
     return (
         <div className='reg-log'>
-            {/* <h1>Register Page</h1> */}
             <div className="reg-log-img" />
             <div className="reg-log-main">
                 <div className="reg-log-main-content">
                     <img src={logo} alt="New Me Logo" style={{width: '40vw', alignSelf: 'center', minWidth: '350px'}} />
                     <hr style={{display: 'block', borderColor: '#ffffff', border: 0, height: '1px', borderTop: '1px solid #1c1c1c'}}/>
                     <div className="reg-log-main-content-heading">
-                        <h1 style={{fontWeight: '500'}}>Create an account</h1>
+                        <h1 style={{fontWeight: '500'}}>Create a new account</h1>
                     </div>
                     <div className="reg-log-main-content-form">
                         <form>
@@ -36,7 +36,7 @@ export default function RegisterPage() {
                     <p style={{lineHeight: 1.5}}>By creating an account, you agree to our<br /><span>Terms of Use</span> and <span>Privacy Policy</span>.</p>
 
                     <button>Sign up</button>
-                    <p>Already have an account? <span>Log in</span></p>
+                    <p>Already have an account? <Link to='/login'><span>Log in</span></Link></p>
                 </div>
             </div>
         </div>
