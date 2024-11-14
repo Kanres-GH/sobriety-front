d026e359df4b1c2968400a207a9fd4f74b7f36c4
Kanres-GH <ktnrgs1817@gmail.com>
2024-11-14 23:34:35 +0500

    added registration page

---
+++ src/components/RegisterPage.tsx
@@ -0,0 +1,44 @@
+import logo from '../static/imgs/logo.svg'
+import '../static/css/reg-log.css'
+
+export default function RegisterPage() {
+
+    return (
+        <div className='reg-log'>
+            {/* <h1>Register Page</h1> */}
+            <div className="reg-log-img" />
+            <div className="reg-log-main">
+                <div className="reg-log-main-content">
+                    <img src={logo} alt="New Me Logo" style={{width: '40vw', alignSelf: 'center', minWidth: '350px'}} />
+                    <hr style={{display: 'block', borderColor: '#ffffff', border: 0, height: '1px', borderTop: '1px solid #1c1c1c'}}/>
+                    <div className="reg-log-main-content-heading">
+                        <h1 style={{fontWeight: '500'}}>Create an account</h1>
+                    </div>
+                    <div className="reg-log-main-content-form">
+                        <form>
+                            <div className="reg-log-main-content-form-input">
+                                <label htmlFor="text">Username:</label>
+                                <input type="text" />
+                                <p style={{fontSize: '12px', color: 'grey'}}>Username should not contain special characters like +, /, %, &, etc.</p>
+                            </div>
+                            
+                            <div className="reg-log-main-content-form-input">
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" />
+                            </div>
+                            <div className="reg-log-main-content-form-input">
+                                <label htmlFor="password">Password:</label>
+                                <input type="password" />
+                                <p style={{fontSize: '12px', color: 'grey'}}>Password has to be at least 8 characters long.</p>
+                            </div>
+                        </form>
+                    </div>
+                    <p style={{lineHeight: 1.5}}>By creating an account, you agree to our<br /><span>Terms of Use</span> and <span>Privacy Policy</span>.</p>
+
+                    <button>Sign up</button>
+                    <p>Already have an account? <span>Log in</span></p>
+                </div>
+            </div>
+        </div>
+    );
+}
